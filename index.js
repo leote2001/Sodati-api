2f052f19dccd38fba005dcc3719f58da230d55fd
leote2001 <116810465+leote2001@users.noreply.github.com>
2024-01-29 15:34:38 -0300

    actualiza index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const conDb = require("./db");
 const soda = require("./insertarSodaAlbums");
 const cerati = require("./insertarCeratiAlbums");
 const routerApi = require("./routes");
@@ -8,34 +7,14 @@ const app = express();
 const port = process.env.port;
 
 app.use(express.json());
-//cerati();
-//soda();
+cerati();
+soda();
+
+app.get("/", (req, res) => {
+    res.send("Welcome!");
+});
 
 routerApi(app);
-app.get("/", async (req, res) => {
-    const sql = "insert into soda_albums (id, title, link, cover_medium, tracklist, release_date, type) values(0, 'ddd', 'ddda', 'aaaaaaa', 'ase', 'asdfdf', 'asdf')";
-
-    const inserta = (sql, con) => {
-        return new Promise((resolve, reject) => {
-            con.query(sql, (err, result) => {
-                if (err) {
-                    console.log(err.message)
-                    reject(err.message)
-                    return;
-                }
-                resolve(result);
-            })
-        })
-    }
-    try {
-        const con = await conDb();
-        const data = await inserta(sql, con);
-        res.json(data);
-    } catch(err) {
-        res.status(500).json({message: err.message});
-    }
-    //res.send("Welcome!");
-});
 
 app.listen(port, () => {
     console.log("Escuchando en el puerto:", port);
