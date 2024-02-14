14c2d6518f7f575aaf4a3fca9190e9d8cd886771
leote2001 <116810465+leote2001@users.noreply.github.com>
2024-02-14 18:32:08 -0300

    archivos eliminados

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const sodaRutas = require("./soda.router");
-const ceratiRutas = require("./cerati.router");
-
-const routerApi = (app) => {
-    app.use("/soda", sodaRutas);
-    app.use("/cerati", ceratiRutas);
-}
-module.exports = routerApi;
