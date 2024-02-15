053c4bcae37465d890366d3b42d47cb65bc6b96a
leote2001 <116810465+leote2001@users.noreply.github.com>
2024-02-14 23:49:52 -0300

    Cambios en index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const conDb = require("./db");
 const soda = require("./insertarSodaAlbums");
 const cerati = require("./insertarCeratiAlbums");
 const routerApi = require("./routes");
@@ -7,11 +8,26 @@ const app = express();
 const port = process.env.port;
 
 app.use(express.json());
-cerati();
-soda();
 
-app.get("/", (req, res) => {
-    res.send("Welcome!");
+//cerati();
+//soda();
+
+app.get("/", async (req, res) => {
+    try {
+        const con = await conDb();
+        const sql = "create table if not exists soda_albums (id int not null primary key, title varchar(100) not null, link varchar(100) not null, cover_medium text not null, tracklist varchar(100) not null, release_date varchar(100) not null, type varchar(100) not null)";
+        con.query(sql, (err, result) => {
+            if (err) {
+                console.error(err.message);
+                res.status(500).json({message: "Error al crear la tabla"});
+                return;
+            }
+            res.send("Tabla creada!");
+        });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).json({ message: "Error de conexión a la base de datos" });
+    }
 });
 
 routerApi(app);
