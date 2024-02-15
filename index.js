5c4fdc4ef8149d82c068c48f70be706501586b84
leote2001 <116810465+leote2001@users.noreply.github.com>
2024-02-15 14:58:52 -0300

    otro intento

--- index.js
+++ index.js
@@ -1,39 +1,42 @@
+const mysql = require('mysql2/promise');
 const express = require("express");
 const conDb = require("./db");
-const soda = require("./insertarSodaAlbums");
-const cerati = require("./insertarCeratiAlbums");
-const routerApi = require("./routes");
 require("dotenv").config();
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-//cerati();
-//soda();
-
-/*app.get("/", async (req, res) => {
+app.get("/", async (req, res) => {
     try {
-        const con = await conDb();
-        const sql = "create table if not exists soda_albums (id int not null primary key, title varchar(100) not null, link varchar(100) not null, cover_medium text not null, tracklist varchar(100) not null, release_date varchar(100) not null, type varchar(100) not null)";
-        con.query(sql, (err, result) => {
-            if (err) {
-                console.error(err.message);
-                res.status(500).json({message: "Error al crear la tabla"});
-                return;
-            }
-            res.send("Tabla creada!");
+        const connection = await mysql.createConnection({
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_DATABASE
         });
+
+        const sql = `
+            CREATE TABLE IF NOT EXISTS soda_albums (
+                id INT NOT NULL PRIMARY KEY,
+                title VARCHAR(100) NOT NULL,
+                link VARCHAR(100) NOT NULL,
+                cover_medium TEXT NOT NULL,
+                tracklist VARCHAR(100) NOT NULL,
+                release_date VARCHAR(100) NOT NULL,
+                type VARCHAR(100) NOT NULL
+            )
+        `;
+        
+        await connection.query(sql);
+        console.log("Tabla 'soda_albums' creada correctamente");
+        res.send("Tabla creada!");
     } catch (err) {
-        console.error(err.message);
+        console.error("Error al crear la tabla 'soda_albums':", err);
         res.status(500).json({ message: "Error de conexión a la base de datos" });
     }
 });
-*/
-app.get("/", (req, res) => {
-    res.send("Hola mundo!");
-});
-routerApi(app);
 
 app.listen(port, () => {
     console.log("Escuchando en el puerto:", port);
