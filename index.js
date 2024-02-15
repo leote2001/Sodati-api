bcbc3b72f9191967c4c136a3031963381b4adaf0
leote2001 <116810465+leote2001@users.noreply.github.com>
2024-02-15 11:40:29 -0300

    index probando

--- index.js
+++ index.js
@@ -1,37 +1,46 @@
 const express = require("express");
 const conDb = require("./db");
-const soda = require("./insertarSodaAlbums");
-const cerati = require("./insertarCeratiAlbums");
+const { insertarSodaAlbums, insertarCeratiAlbums } = require("./insertarAlbums");
 const routerApi = require("./routes");
 require("dotenv").config();
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-//cerati();
-//soda();
-
-app.get("/", async (req, res) => {
+// Función para crear la tabla 'soda_albums' si no existe
+async function crearTablaSodaAlbums() {
     try {
         const con = await conDb();
-        const sql = "create table if not exists soda_albums (id int not null primary key, title varchar(100) not null, link varchar(100) not null, cover_medium text not null, tracklist varchar(100) not null, release_date varchar(100) not null, type varchar(100) not null)";
-        con.query(sql, (err, result) => {
-            if (err) {
-                console.error(err.message);
-                res.status(500).json({message: "Error al crear la tabla"});
-                return;
-            }
-            res.send("Tabla creada!");
-        });
+        const sql = `
+            CREATE TABLE IF NOT EXISTS soda_albums (
+                id INT NOT NULL PRIMARY KEY,
+                title VARCHAR(100) NOT NULL,
+                link VARCHAR(100) NOT NULL,
+                cover_medium TEXT NOT NULL,
+                tracklist VARCHAR(100) NOT NULL,
+                release_date VARCHAR(100) NOT NULL,
+                type VARCHAR(100) NOT NULL
+            )
+        `;
+        await con.query(sql);
+        console.log("Tabla 'soda_albums' creada correctamente");
     } catch (err) {
-        console.error(err.message);
-        res.status(500).json({ message: "Error de conexión a la base de datos" });
+        console.error("Error al crear la tabla 'soda_albums':", err.message);
     }
+}
+
+// Rutas
+app.get("/", async (req, res) => {
+    await crearTablaSodaAlbums(); // Crear la tabla 'soda_albums' antes de procesar las solicitudes
+    res.send("¡Bienvenido!");
 });
 
-//routerApi(app);
+// Manejo de rutas API
+routerApi(app);
 
-app.listen(port, "0.0.0.0", () => {
+// Iniciar el servidor
+app.listen(port, () => {
     console.log("Escuchando en el puerto:", port);
 });
